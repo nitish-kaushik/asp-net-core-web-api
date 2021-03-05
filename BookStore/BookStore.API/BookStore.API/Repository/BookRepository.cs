b75ddb5a465be2912d270184176bdaac7d88d3ac
Nitish <nitish.webgentle@gmail.com>
2021-03-05 21:10:50 +0530

    Added BookStore Project

---
+++ BookStore/BookStore.API/BookStore.API/Repository/BookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+    }
+}
