b34dcdf36c0ab265bef0cf09dcc89776c27874cc
Nitish <nitish.webgentle@gmail.com>
2021-03-07 22:19:46 +0530

    Model validation

--- BookStore/BookStore.API/BookStore.API/Models/BookModel.cs
+++ BookStore/BookStore.API/BookStore.API/Models/BookModel.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.API.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please Add title property")]
+      
         public string Title { get; set; }
         public string Description { get; set; }
     }
